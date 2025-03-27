e1c1211ebddf421e325d6d664134c369afcd6a58
Cristian Navarro Gonzalez <cristianskate82@hotmail.com>
2025-03-27 10:16:31 -0600

    feature/setup-navigation
    - Adding navigation library
    - Setup navigation config

--- app/src/main/java/com/app/hackernews/ui/home/viewModel/HomeScreenViewModel.kt
+++ app/src/main/java/com/app/hackernews/ui/home/viewModel/HomeScreenViewModel.kt
@@ -2,8 +2,10 @@ package com.app.hackernews.ui.home.viewModel
 
 import androidx.lifecycle.ViewModel
 import com.app.hackernews.domain.AndroidNewsUseCase
+import dagger.hilt.android.lifecycle.HiltViewModel
 import javax.inject.Inject
 
+@HiltViewModel
 class HomeScreenViewModel
     @Inject
     constructor(
