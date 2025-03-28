6acbcd484ce77430e5dc24ed59e38044a6561410
Cristian Navarro Gonzalez <cristianskate82@hotmail.com>
2025-03-27 22:03:44 -0600

    feature/database-implementation
    - Database implementation
    - Save data when server give us code 200
    - Loading data when you don't have internet connection

---
+++ app/src/main/java/com/app/hackernews/data/database/DatabaseConstants.kt
@@ -0,0 +1,4 @@
+package com.app.hackernews.data.database
+
+const val NEWS_DATABASE = "NEWS_DATABASE"
+const val NEWS_TABLE = "NEWS_TABLE"
---
+++ app/src/main/java/com/app/hackernews/data/database/dao/NewsDao.kt
@@ -0,0 +1,16 @@
+package com.app.hackernews.data.database.dao
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import com.app.hackernews.data.database.entities.NewsEntity
+
+@Dao
+interface NewsDao {
+    @Insert(onConflict = OnConflictStrategy.REPLACE)
+    suspend fun insertAll(newsList: List<NewsEntity>)
+
+    @Query("SELECT * FROM NEWS_TABLE")
+    suspend fun getAllNews(): List<NewsEntity>
+}
