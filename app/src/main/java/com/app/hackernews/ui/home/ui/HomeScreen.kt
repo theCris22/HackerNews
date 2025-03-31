7f48720e1aeea018d9aa72602408c4916ed9a855
Cristian Navarro Gonzalez <cristianskate82@hotmail.com>
2025-03-31 16:14:00 -0600

    chore/refactor-code
    Reorganized code

--- app/src/main/java/com/app/hackernews/ui/home/ui/HomeScreen.kt
+++ app/src/main/java/com/app/hackernews/ui/home/ui/HomeScreen.kt
@@ -11,9 +11,13 @@ import androidx.fragment.app.viewModels
 import androidx.lifecycle.Lifecycle
 import androidx.lifecycle.lifecycleScope
 import androidx.lifecycle.repeatOnLifecycle
+import androidx.recyclerview.widget.ItemTouchHelper
+import androidx.recyclerview.widget.RecyclerView
 import com.app.hackernews.R
+import com.app.hackernews.core.openCustomTab
 import com.app.hackernews.data.network.models.Hit
 import com.app.hackernews.data.network.models.RequestState
+import com.app.hackernews.data.network.response.AndroidNewsResponse
 import com.app.hackernews.databinding.HomeScreenBinding
 import com.app.hackernews.ui.home.adapters.HackerNewsAdapter
 import com.app.hackernews.ui.home.viewModel.HomeScreenViewModel
@@ -25,8 +29,8 @@ class HomeScreen : Fragment(R.layout.home_screen) {
     private var _binding: HomeScreenBinding? = null
     private val binding get() = _binding!!
 
-    private val adapter by lazy { HackerNewsAdapter(requireContext(), onItemClick = { itemClick(it) }) }
     private lateinit var customTabLauncher: ActivityResultLauncher<Intent>
+    private val adapter by lazy { HackerNewsAdapter(requireContext(), onItemClick = { itemClick(it) }) }
     private val viewModel: HomeScreenViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -68,32 +72,60 @@ class HomeScreen : Fragment(R.layout.home_screen) {
             lifecycle.repeatOnLifecycle(Lifecycle.State.STARTED) {
                 viewModel.androidNewsState.collect { state ->
                     when (state) {
-                        is RequestState.Success -> handleSuccess(state.data.hits)
-                        is RequestState.Error -> handleError(state.data?.hits ?: emptyList())
-                        RequestState.Idle -> handleState(false)
-                        RequestState.Loading -> handleState(true)
+                        is RequestState.Success -> {
+                            handleSuccess(state)
+                        }
+                        is RequestState.Error -> {
+                            handleError(state)
+                        }
+                        RequestState.Idle -> handleLoading(false)
+                        RequestState.Loading -> handleLoading(true)
                     }
                 }
             }
         }
     }
 
-    private fun handleSuccess(data: List<Hit>) {
-        handleState(false)
-        adapter.updateData(data)
+    private fun handleSuccess(state: RequestState.Success<AndroidNewsResponse>) {
+        handleLoading(false)
+        adapter.submitList(state.data.hits)
+        swipeToDelete(binding.recyclerNews)
     }
 
-    private fun handleError(data: List<Hit>) {
-        handleState(false)
-        adapter.updateData(data)
+    private fun handleError(state: RequestState.Error<AndroidNewsResponse>) {
+        handleLoading(false)
+        adapter.submitList(state.data?.hits)
+        swipeToDelete(binding.recyclerNews)
     }
 
-    private fun handleState(isLoading: Boolean) {
+    private fun handleLoading(isLoading: Boolean) {
         binding.swipeRefreshLayout.isRefreshing = isLoading
     }
 
     private fun itemClick(hit: Hit) {
-        hit.storyUrl?.let { viewModel.openCustomTab(it, customTabLauncher) }
+        hit.storyUrl?.let { openCustomTab(it, customTabLauncher) }
             ?: run { Toast.makeText(requireContext(), getString(R.string.not_url), Toast.LENGTH_SHORT).show() }
+    }
+
+    private fun swipeToDelete(recyclerView: RecyclerView) {
+        val itemTouchHelper =
+            object : ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT) {
+                override fun onMove(
+                    recyclerView: RecyclerView,
+                    viewHolder: RecyclerView.ViewHolder,
+                    target: RecyclerView.ViewHolder,
+                ): Boolean = false
+
+                override fun onSwiped(
+                    viewHolder: RecyclerView.ViewHolder,
+                    direction: Int,
+                ) {
+                    val pos = viewHolder.adapterPosition
+                    val hitToDelete = adapter.currentList[pos]
+                    viewModel.removeItem(hitToDelete)
+                }
+            }
+
+        ItemTouchHelper(itemTouchHelper).attachToRecyclerView(recyclerView)
     }
 }
